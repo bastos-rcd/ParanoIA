1c63ab36867d0d3abb7f31a844a8804b47e87e1d
Bastien <record.bastien@gmail.com>
2024-09-18 12:41:43 +0200

    feat: 404 error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { UserGameComponent } from './user/user-game/user-game.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', component: UserGameComponent }
+  { path: '', component: UserGameComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
