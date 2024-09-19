1d02fd1d8e9c530decb84c8c3b6f2ea9c9d80e9f
Bastien <record.bastien@gmail.com>
2024-09-19 18:56:46 +0200

    feat: task5

---
+++ src/app/game/task-5/task-5.component.ts
@@ -0,0 +1,105 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { UpdateComponent } from '../update/update.component';
+import { environment } from '../../../environments/environment';
+
+@Component({
+  selector: 'app-task-5',
+  standalone: true,
+  imports: [
+    CommonModule,
+    UpdateComponent
+  ],
+  templateUrl: './task-5.component.html',
+  styleUrl: './task-5.component.css'
+})
+
+export class Task5Component {
+  @Input()
+  public numTask: number = 0;
+
+  public isWin: boolean = false;
+
+  public blue: boolean = false;
+  public red: boolean = false;
+  public orange: boolean = false;
+  public green: boolean = false;
+
+  public isPlaying: boolean = false;
+
+  private order: string[] = [];
+  public result: string[] = [
+    environment.randomChoice(['blue', 'red', 'orange', 'green']),
+    environment.randomChoice(['blue', 'red', 'orange', 'green']),
+    environment.randomChoice(['blue', 'red', 'orange', 'green']),
+    environment.randomChoice(['blue', 'red', 'orange', 'green'])
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.showOrder();
+  }
+
+  private showOrder() {
+    this.isPlaying = true;
+
+    setTimeout(() => {
+      this.playColor(this.result[0]);
+    }, 1000);
+    setTimeout(() => {
+      this.hideColor();
+    }, 2000);
+    setTimeout(() => {
+      this.playColor(this.result[1]);
+    }, 3000);
+    setTimeout(() => {
+      this.hideColor();
+    }, 4000);
+    setTimeout(() => {
+      this.playColor(this.result[2]);
+    }, 5000);
+    setTimeout(() => {
+      this.hideColor();
+    }, 6000);
+    setTimeout(() => {
+      this.playColor(this.result[3]);
+    }, 7000);
+    setTimeout(() => {
+      this.hideColor();
+      this.isPlaying = false;
+    }, 8000);
+  }
+
+  private playColor(color: string) {
+    if (color === 'blue') {
+      this.blue = true;
+    } else if (color === 'red') {
+      this.red = true;
+    } else if (color === 'orange') {
+      this.orange = true;
+    } else if (color === 'green') {
+      this.green = true;
+    }
+  }
+
+  private hideColor() {
+    this.blue = false;
+    this.red = false;
+    this.orange = false;
+    this.green = false;
+  }
+
+  public clickColor(color: string) {
+    this.order.push(color);
+
+    if (this.order.length === this.result.length) {
+      if (JSON.stringify(this.order) === JSON.stringify(this.result)) {
+        this.isWin = true;
+      } else {
+        this.order = [];
+        this.showOrder();
+      }
+    }
+  }
+}
--- src/app/game/task/task.component.ts
+++ src/app/game/task/task.component.ts
@@ -8,6 +8,7 @@ import { Task1Component } from '../task-1/task-1.component';
 import { Task2Component } from '../task-2/task-2.component';
 import { Task3Component } from '../task-3/task-3.component';
 import { Task4Component } from '../task-4/task-4.component';
+import { Task5Component } from '../task-5/task-5.component';
 
 @Component({
   selector: 'app-task',
@@ -18,6 +19,7 @@ import { Task4Component } from '../task-4/task-4.component';
     Task2Component,
     Task3Component,
     Task4Component,
+    Task5Component,
     CommonModule
   ],
   templateUrl: './task.component.html',
